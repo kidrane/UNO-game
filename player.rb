af9c57f11e39bf86b1c9d440b6f8381fbe22bf5f
Mars Cheng <kidrane@gmail.com>
2010-07-29 15:50:53 +0800

    Player基本完成，可以打牌，摸牌，判定输赢

---
+++ player.rb
@@ -0,0 +1,42 @@
+require "deck"
+
+class Player
+
+  attr_reader :player_name
+  attr_reader :cards
+
+  def initialize player_name, deck = Deck.new
+    @player_name = player_name
+    @deck = deck
+    @cards = Array.new
+  end
+  
+  def inspect
+    @player_name
+  end
+
+  def fetch number = 1
+    number.times do
+      card = @deck.fetch
+      @cards << card
+    end
+    card
+  end
+
+  def count
+    @cards.count
+  end
+
+  def play index = 0
+    @deck.recycle << @cards[index]
+    @cards.delete_at index
+  end
+  
+  def pass
+  end
+
+  def win?
+    count == 0
+  end
+
+end

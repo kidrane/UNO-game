e8024910ee326f477083d43f859d7257aa0318ba
Mars Cheng <kidrane@gmail.com>
2010-07-29 12:51:07 +0800

    添加了Deck类，可以生成新牌，可以在没有牌的的时候自动洗牌

--- card_spec.rb
+++ card_spec.rb
@@ -1,11 +1,25 @@
 require "card"
 
 describe Card do
+
+  before do
+    @card = Card.new :color => :black, :type => :wild
+  end
   
   it "应该能设置花色和牌的类别" do
-    card = Card.new :color => :black, :type => :plus2
-    card.color.should == :black
-    card.type.should == :plus2
+    @card.color.should == :black
+    @card.type.should == :wild
+  end
+
+  it "应该显示自己的花色和类别" do
+    @card.inspect.should == "black wild"
+  end
+
+  it "应该判别牌的合法性" do
+    valid_card = Card.new :color => :red, :type => 9
+    invalid_card = Card.new :color => :white, type => :ten
+    valid_card.valid?.should == true
+    invalid_card.valid?.should == false
   end
 
 end

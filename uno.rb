c0041aae0f4144072d58ae234b5532ef54bc9eaa
Mars Cheng <kidrane@gmail.com>
2010-07-29 22:33:03 +0800

    add some basic actions

--- uno.rb
+++ uno.rb
@@ -14,6 +14,7 @@ class Uno
     @recycle = Recycle.new
     @deck = Deck.new @recycle
     @players = Array.new
+    @have_to_wild = 0
   end
 
   def add_player player
@@ -41,4 +42,25 @@ class Uno
     return fetch_a_starting_card if @current_color == :black
     card
   end
+
+  def vaild_cards
+  end
+
+  def reverse
+    @players.reverse!
+  end
+
+  def next_player
+    @players << current_player
+    @current_player = @players.shift
+  end
+  
+  def current_player
+    @current_player ||= @players.shift
+  end
+
+  def wild
+    @have_to_wild += 2
+  end
+
 end
